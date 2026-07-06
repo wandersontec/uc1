32522cb5b3c58c1c29dc49708fcb1eaf5c10d034
wandersontec <wandersontec@gmail.com>
2026-07-06 20:51:56 -0300

    prog25.py alt

--- prog25.py
+++ prog25.py
@@ -1,6 +1,6 @@
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
-if num1>num2
-print(f"o valor de {num1} é maior que {num2}")
+if num1>num2:
+    print(f"o valor de {num1} é maior que {num2}")
 else:
     print(f"o valor de {num2} é maior que {num1}")
